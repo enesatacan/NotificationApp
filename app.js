d09841ec9c0a8080ba20f83505a32741c013cae5
Enes ATACAN <101789777+enesatacan@users.noreply.github.com>
2025-01-06 13:25:22 +0300

    New Project

---
+++ app.js
@@ -0,0 +1,23 @@
+let toastBox = document.getElementById("toastBox");
+let successMsg = "<i class='fa-solid fa-check'></i> Successfully submited";
+let errorMsg = "<i class='fa-solid fa-xmark'></i> Please fix the error!";
+let invalidMsg =
+  "<i class='fa-solid fa-exclamation'></i> Invalid input, check again";
+
+function showToast(msg) {
+  let toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerHTML = msg;
+  toastBox.appendChild(toast);
+
+  if (msg.includes("error")) {
+    toast.classList.add("error");
+  }
+  if (msg.includes("Invalid")) {
+    toast.classList.add("invalid");
+  }
+
+  setTimeout(() => {
+    toast.remove();
+  }, 6000);
+}
